34e12b7884855856afa7fcd3c3329c5dfe85da05
KuukuDev <owurakubuabeng777@gmail.com>
2023-06-25 07:45:37 +0000

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file in /tmp using Puppet
+
+file { 'holberton':
+  ensure  => present,
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
